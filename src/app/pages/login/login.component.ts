181af6f6f21b79b0804882675afa81c7b63dc395
jpmataro <jpmataro@gmail.com>
2020-06-01 16:51:14 +0200

    Change Materialize to Bulma

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+
+  constructor( private formBuilder: FormBuilder) { 
+    this.formCreate();
+  }
 
   ngOnInit(): void {
+  }
+
+  get userValid() {
+    return this.loginForm.get('user').invalid && this.loginForm.get('user').touched;
+  }
+
+  get passValid() {
+    return this.loginForm.get('password').invalid && this.loginForm.get('password').touched;
+  }
+
+  formCreate() {
+    this.loginForm = this.formBuilder.group({
+      user:["", Validators.required],
+      password:["", Validators.required],
+    });
+  }
+
+  login() {
+    console.log(this.loginForm);
+    if(this.loginForm.invalid) {
+      return Object.values(this.loginForm.controls).forEach(control => {
+        control.markAsTouched();
+      })
+    }
   }
 
 }
